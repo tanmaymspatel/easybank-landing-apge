3aca04eb7f0c6611b3f4b3bf54a7cc02c94dc0a1
Tanmay Patel <tanmay.1rivet@gmail.com>
2023-01-23 19:03:58 +0530

    fix: hero section image

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,7 +4,7 @@ function HeroSection() {
     return (
         <section className='bg-warning'>
             <div className="container">
-                <div className="row justify-content-between align-items-center hero-bkg">
+                <div className="row justify-content-between align-items-center hero-bkg position-relative">
                     <div className="col-md-6">
                         <h1 className='hero-heading text-primary'>Next generation digital banking</h1>
                         <p className='hero-sub-heading text-success py-3'>Take your financial online. Your easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more. </p>
@@ -13,8 +13,8 @@ function HeroSection() {
                         </div>
                     </div>
                     <div className="col-md-6">
-                        <figure className="mb-0">
-                            <img src={heroBkg} alt="Hero Section" />
+                        <figure className="mb-0 ">
+                            <img className='position-absolute hero-img' src={heroBkg} alt="Hero Section" />
                         </figure>
                     </div>
                 </div>
